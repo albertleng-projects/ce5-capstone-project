7b0dafb4a02d6231e26b84830cc345536e9c69b1
Leng Zai <albertleng@hotmail.com>
2024-06-08 12:00:01 +0800

    ALB-23: Add helm charts and commands

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,18 +3,24 @@
 # TODOs:
 # 1. Replace the account id with environment variable
 # 2. Replace the region with environment variable
+# 3. Add environment and version to the Docker images, for example: chatbot-`staging`:`1.0.0` (using environment variables)
+
+# shellcheck disable=SC2269
+ENVIRONMENT=$ENVIRONMENT
+VERSION=$VERSION
 
 # Build the Docker images
-sudo docker build -t chatbot-app ./chatbot
-sudo docker build -t sentiment-analysis-api ./api
+sudo docker build -t chatbot-"$ENVIRONMENT":"$VERSION" ./chatbot
+sudo docker build -t sentiment-analysis-api-"$ENVIRONMENT":"$VERSION" ./api
 
 # Authenticate Docker to the ECR registry
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 255945442255.dkr.ecr.us-east-1.amazonaws.com
 
 # Tag the Docker images
-docker tag chatbot-app:latest 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr:chatbot-app-latest
-docker tag sentiment-analysis-api:latest 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr:sentiment-analysis-api-latest
+docker tag chatbot-"$ENVIRONMENT":"$VERSION" 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-chatbot-"$ENVIRONMENT":"$VERSION"
+# shellcheck disable=SC2086
+docker tag sentiment-analysis-api-"$ENVIRONMENT":"$VERSION" 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-api-$ENVIRONMENT:$VERSION
 
 # Push the Docker images
-docker push 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr:chatbot-app-latest
-docker push 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr:sentiment-analysis-api-latest
+docker push 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-chatbot-"$ENVIRONMENT":"$VERSION"
+docker push 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-api-"$ENVIRONMENT":"$VERSION"
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,3 @@
-# https://learnk8s.io/terraform-eks
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -54,53 +52,6 @@ resource "local_file" "kubeconfig" {
   filename = "kubeconfig_${local.cluster_name}"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
-
-  name = "ce5-group6-vpc"
-  cidr = "172.16.0.0/16"
-  azs  = data.aws_availability_zones.available.names
-
-  #   https://github.com/hashicorp/terraform-provider-aws/issues/23488
-  #   TODO: Fix
-  #   module.vpc.aws_subnet.public[0]: Still destroying... [id=subnet-0ea022eb6028a9427, 20m0s elapsed]
-  #module.vpc.aws_internet_gateway.this[0]: Still destroying... [id=igw-0c7d70ca564a8f6d7, 20m0s elapsed]
-  #╷
-  #│ Error: deleting EC2 Subnet (subnet-0ea022eb6028a9427): DependencyViolation: The subnet 'subnet-0ea022eb6028a9427' has dependencies and cannot be deleted.
-  #│       status code: 400, request id: 799c4c09-d035-4342-98e9-d7cbe44b2d1f
-  #│
-  #│
-  #╵
-  #╷
-  #│ Error: deleting EC2 Subnet (subnet-04e04164ef5963f1e): DependencyViolation: The subnet 'subnet-04e04164ef5963f1e' has dependencies and cannot be deleted.
-  #│       status code: 400, request id: 60c53453-c38a-4348-8ee8-ed4b6679fac4
-  #│
-  #│
-  #╵
-  #╷
-  #│ Error: deleting EC2 Internet Gateway (igw-0c7d70ca564a8f6d7): detaching EC2 Internet Gateway (igw-0c7d70ca564a8f6d7) from VPC (vpc-053c9eaae00d398dc): DependencyViolation: Network vpc-053c9eaae00d398dc has some mapped public address(es). Please unmap those public address(es) before detaching the gateway.
-  #│       status code: 400, request id: c640edeb-4b6a-4635-909c-c930cf1645cf
-  #│
-  #│
-  #╵
-
-  private_subnets = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  public_subnets = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
-}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -108,9 +59,12 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   cluster_version = "1.30"
-  subnet_ids      = module.vpc.private_subnets
+  subnet_ids = [
+    "subnet-0aa24ba6afc315287", "subnet-0df58e4c0dd0fab5f",
+    "subnet-0334d23b810a18cb0"
+  ]
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id = "vpc-0b1601b5d13b56128"
 
   eks_managed_node_groups = {
     ce5-group6-node_group = {
@@ -119,8 +73,6 @@ module "eks" {
       min_capacity     = 1
 
       instance_type = "t3.medium"
-      #       TODO: Use bastion host instead
-      #       public_ip     = true
     }
   }
 }
