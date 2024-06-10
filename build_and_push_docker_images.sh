21ac28adb9d9f8d03f5d9fae511e1923caf4a1d8
Leng Zai <albertleng@hotmail.com>
2024-06-10 14:39:46 +0800

    ALB-23: Update to use helm values to store 'account_id' for reusability

--- build_and_push_docker_images.sh
+++ build_and_push_docker_images.sh
@@ -3,21 +3,24 @@
 
 ENVIRONMENT=$(grep 'environment:' ./helm/values.yaml | awk '{print $2}')
 VERSION=$(grep 'version:' ./helm/values.yaml | awk '{print $2}')
+ACCOUNT_ID=$(grep 'account_id:' ./helm/values.yaml | awk '{print $2}')
 
 export ENVIRONMENT
 export VERSION
+export ACCOUNT_ID
 
 echo "export ENVIRONMENT=${ENVIRONMENT}"
 echo "export VERSION=${VERSION}"
+echo "export ACCOUNT_ID=${ACCOUNT_ID}"
 
 # Build and push Docker images
 sudo docker build -t chatbot-"$ENVIRONMENT":"$VERSION" ./chatbot
 sudo docker build -t sentiment-analysis-api-"$ENVIRONMENT":"$VERSION" ./api
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 255945442255.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com
 
-docker tag chatbot-"$ENVIRONMENT":"$VERSION" 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-chatbot-"$ENVIRONMENT":"$VERSION"
-docker tag sentiment-analysis-api-"$ENVIRONMENT":"$VERSION" 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-api-"$ENVIRONMENT":"$VERSION"
+docker tag chatbot-"$ENVIRONMENT":"$VERSION" "$ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-chatbot-"$ENVIRONMENT":"$VERSION"
+docker tag sentiment-analysis-api-"$ENVIRONMENT":"$VERSION" "$ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-api-"$ENVIRONMENT":"$VERSION"
 
-docker push 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-chatbot-"$ENVIRONMENT":"$VERSION"
-docker push 255945442255.dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-api-"$ENVIRONMENT":"$VERSION"
+docker push "$ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-chatbot-"$ENVIRONMENT":"$VERSION"
+docker push "$ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/ce5-group6-ecr-api-"$ENVIRONMENT":"$VERSION"
